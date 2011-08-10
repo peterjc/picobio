425aa7f8b9201c83fb042f55ccc49e9f5482ce82
peterjc <p.j.a.cock@googlemail.com>
2011-08-10 14:38:45 +0100

    Expand nal/pal alias files to sync files which may be in different directories

--- blast/blast_sync.py
+++ blast/blast_sync.py
@@ -13,8 +13,7 @@ Return codes:
 2 - Locked, aborted
 3 - Failed
 
-TODO - Different return code when already up to date
-TODO - Automatically expand pal and nal alias files...
+TODO - Different return code when already up to date?
 """
 #master = "/data/blastdb"
 #local = "/tmp/galaxy-blastdb"
@@ -23,6 +22,27 @@ TODO - Automatically expand pal and nal alias files...
 import os
 import sys
 import time
+
+try:
+    from os.path import relpath
+except ImportError:
+    #Must be prior to Python 2.6
+    #This implementation is based on that by James Gardner in
+    #MIT licensed package BareNecessities
+    import posixpath
+    def relpath(path, start=posixpath.curdir):
+        """Return a relative version of a path"""
+        if not path:
+            raise ValueError("no path specified")
+        start_list = posixpath.abspath(start).split(posixpath.sep)
+        path_list = posixpath.abspath(path).split(posixpath.sep)
+        # Work out how much of the filepath is shared by start and path.
+        i = len(posixpath.commonprefix([start_list, path_list]))
+        rel_list = [posixpath.pardir] * (len(start_list)-i) + path_list[i:]
+        if not rel_list:
+            return curdir
+        return posixpath.join(*rel_list)
+    assert relpath("/data/blast/ncbi/nr.pal", "/data/blast") == "ncbi/nr.pal"
 
 if len(sys.argv) < 4:
     sys.stderr.write("Bad BLAST database sync arguments\n")
@@ -40,12 +60,34 @@ if not os.path.isdir(master):
 if not os.path.isdir(local):
     os.makedirs(local, 0777)
 
+def sync_blast_alias_db(master, local, db, index):
+    print "Syncing %s" % index
+    handle = open(index)
+    for line in handle:
+        if line.startswith("DBLIST "):
+            dbs = line[7:].strip().split()
+            for d in dbs:
+                d = os.path.join(os.path.split(index)[0], d)
+                err = sync_blast_db(master, local, relpath(d, master))
+                if err:
+                    return err
+    handle.close()
+    return 0
+
 def sync_blast_db(master, local, db):
+    for index in [os.path.join(master, db + ".nal"),
+                  os.path.join(master, db + ".pal")]:
+        if os.path.isfile(index):
+            err = sync_blast_alias_db(master, local, db, index)
+            if err:
+                return err
+            #else continue to sync the alias file itself,
+
     cmd = "rsync -v -rtz --exclude=*.tar.gz --exclude=*.md5 %s %s"
     old = os.path.join(master, db + ".*")
     new = os.path.join(local, os.path.split(db)[0]) #Folder namer!
     #print "%s -> %s" % (old, new)
-    #print cmd % (old, new)
+    print cmd % (old, new)
     err = os.system(cmd % (old, new))
     if err:
         sys.stderr.write("Return code %i from rsync:\n%s\n" % (err, cmd % (old, new)))
@@ -80,7 +122,7 @@ for db in names:
         sys.exit(2)
     try:
         handle = open(lock, 'w')
-        handle.write(time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()))
+        handle.write(time.strftime("%a, %d %b %Y %H:%M:%S +0000\n", time.gmtime()))
         handle.close()
     except:
         sys.stderr.write("Could not create BLAST DB lock\n")
