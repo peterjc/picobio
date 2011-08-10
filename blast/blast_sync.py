dc5b8953a1b056ce0316ae3c6e6149333695c5aa
peterjc <p.j.a.cock@googlemail.com>
2011-08-10 11:13:49 +0100

    Adding basic BLAST database sync/cache wrapper scripts

---
+++ blast/blast_sync.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+"""Sync BLAST database(s)
+
+Arguments:
+
+(*) Master path, e.g. /data/blastdb
+(*) Local path, e.g. /tmp/galaxy-blastdb
+(*) Database name(s), e.g. ncbi/nr
+
+Return codes:
+
+0 - Worked, up to date
+2 - Locked, aborted
+3 - Failed
+
+TODO - Wait and retry if lock exists (consider multiple jobs running
+       on the same cluster node at once)
+TODO - Try single call to rsync with wild card (exclude tar balls)
+TODO - Different return code when already up to date
+TODO - Automatically expand pal and nal alias files...
+"""
+#master = "/data/blastdb"
+#local = "/tmp/galaxy-blastdb"
+#db = "ncbi/nr"
+
+import os
+import sys
+import time
+
+if len(sys.argv) < 4:
+    sys.stderr.write("Bad BLAST database sync arguments\n")
+    sys.stderr.write("Expect source path, dest path, one or more DBs\n")
+    sys.exit(3)
+
+master = sys.argv[1]
+local = sys.argv[2]
+names = sys.argv[3:]
+
+if not os.path.isdir(master):
+    sys.stderr.write("Master directory %s not found\n" % master)
+    sys.exit(3)
+
+if not os.path.isdir(local):
+    os.makedirs(local, 0777)
+
+n_ext = [".nal", ".nhd", ".nhi", ".nhr", ".nin", ".nnd", ".nni",
+         ".nog", ".nsd", ".nsi", ".nsq"]
+p_ext = [".pal", ".phd", ".phi", ".phr", ".pin", ".pnd", ".pni",
+         ".pog", ".ppd", ".ppi", ".psd", ".psi", ".psq"]
+
+cmd = "rsync -v -rtz %s %s"
+
+def file_list(master, local, db):
+    d = os.path.split(db)[0]
+    directory, name = os.path.split(os.path.join(master, db))
+    for f in os.listdir(directory):
+        if f.startswith(name + "."):
+            ext = os.path.splitext(f)[-1]
+            if ext in n_ext or ext in p_ext:
+                yield os.path.join(master, d, f), os.path.join(local, d, f)
+
+
+for db in names:
+    print db
+    lock = os.path.join(local, db + ".lock")
+    if not os.path.isdir(os.path.split(lock)[0]):
+        os.makedirs(os.path.split(lock)[0], 0777)
+    if os.path.isfile(lock):
+        sys.stderr.write("BLAST Database already locked:\n")
+        try:
+            handle = open(lock)
+            sys.stderr.write(handle.read())
+            handle.close()
+        except:
+            pass
+        sys.stderr.write("\nAborting sync\n")
+        sys.exit(2)
+    try:
+        handle = open(lock, 'w')
+        handle.write(time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()))
+        handle.close()
+    except:
+        sys.stderr.write("Could not create BLAST DB lock\n")
+        sys.exit(2)
+
+    for old, new in file_list(master, local, db):
+        print "%s -> %s" % (old, new)
+        err = os.system(cmd % (old, new))
+        if err:
+            sys.stderr("Return code %i from rsync:\n%s\n" % (err, cmd % (old, new)))
+            os.remove(lock)
+            sys.exit(3)
+    os.remove(lock)
+    print "%s done" % db
+print "Done"
---
+++ blast/blast_wrap.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+"""Simple Python wrapper for BLAST to locally cache databases.
+
+This is intended for use withing a computer cluster where there
+is a central copy of the BLAST databases which is updated regularly,
+but which is slow to access over the network. Therefore for speed,
+and assuming sufficient disk space on each node, we want to copy
+the BLAST databases to a local cache.
+
+This can be setup as a scheduled task (e.g. with cron) for key
+databases, and/or uses via a wrapper script to sync on demand
+(see blast_wrap.py).
+
+Currently uses two hard coded settings for the network mounted
+master copy of the databases (e.g. NFS, SAMBA), and the local
+fast hard drive to use as the cache.
+
+We're using /data/blastdb as the master, and /tmp/galaxy-blastdb
+as the local cache.
+
+Intention is rather than this:
+
+$ blastx -query=example.fasta -db=/data/blastdb/ncbi/nr ...
+
+You do this:
+
+$ blast_wrap.py blastx -query=example.fasta -db=/data/blastdb/ncbi/nr ...                         
+This will cache the /data/blastdb/nr.* files as /tmp/galaxy-blastdb/ncbi/nr.*
+and run this command for you:
+
+$ blastx -query=example.fasta -db=/tmp/galaxy-blastdb/ncbi/nr ...
+
+TODO: Work out the database path if not given explicitly (e.g. just nr)
+but via the BLAST environment variable etc.
+"""
+
+master = "/data/blastdb"
+local = "/tmp/galaxy-blastdb"
+#db = "ncbi/nr"
+
+import sys
+import os
+
+#argv[0] is this python script
+#Turn the argv list into a string, escaping as needed
+def wrap(text):
+    if " " in text and not text[0]=='"' and not text[-1]=='"':
+        return '"%s"' % text
+    else:
+        return text
+cmd = " ".join(wrap(arg) for arg in sys.argv[1:])
+
+if master in cmd:
+    #We have syncing to do!
+    i = cmd.find(master + "/")
+    db = cmd[i+len(master)+1:].split(None,1)[0]
+    if db.endswith('"'):
+        db = db.rstrip('"')
+    print "Synchronising %s ..." % db
+    assert master + "/" + db in cmd
+    err = os.system("blast_sync.py %s %s %s > /dev/null" % (master, local, db))
+    if err:
+        sys.stderr.write("Syncing %s failed (error code %i)\n" % (db, err))
+        sys.exit(err)
+    #Update the command
+    cmd = cmd.replace(master + "/"+ db,local +"/" + db)
+    print "Synchronised %s" % db
+        
+#Run the command
+sys.exit(os.system(cmd))
