c99d530cd9be155df917788ea8a74573c99fcfd6
peterjc <p.j.a.cock@googlemail.com>
2012-03-08 15:57:03 +0000

    Adding blacklist mode to sam_strip_tags.py

--- sambam/sam_strip_tags.py
+++ sambam/sam_strip_tags.py
@@ -3,8 +3,8 @@
 
 This script is designed to be used as part of a Unix pipeline. It
 takes as optional command line arguments a white list of tags to
-preserve. It reads SAM format data from stdin, and writes SAM format
-data to stdout.
+preserve (or a black list). It reads SAM format data from stdin,
+and writes SAM format data to stdout.
 
 The only change made to the SAM reads is in the SEQ field of mapped
 reads. Any bases matching the reference are replaced with equals
@@ -14,24 +14,45 @@ and without the equals, or their BAM equivalents.
 
 Simple usage with SAM files, keeping only read-group tags:
 
-$ ./sam_strip_tags.py RG < original.sam > equals.sam
+$ ./sam_strip_tags.py RG < original.sam > only_RG.sam
 
 Simple usage with BAM files with conversion to/from SAM via samtools:
 
-$ samtools view -h original.bam | ./sam_strip_tags.py RG | samtools view -S -b - > equals.bam
+$ samtools view -h original.bam | ./sam_strip_tags.py RG | samtools view -S -b - > only_RG.bam
 
 If your SAM/BAM files lack @SQ headers, you may need to give
 samtools the reference FASTA file as well.
+
+To remove particular tags (a black list rather than a white list)
+include the switch -v (for invert, like the grep option). For example,
+to remove any original quality (OC) tags, use:
+
+$ ./sam_strip_tags.py -v OQ < original.sam > no_OQ.sam
+
+Likewise with BAM files via samtools,
+
+$ samtools view -h original.bam | ./sam_strip_tags.py -v OQ | samtools view -S -b - > no_OQ.bam
 """
 
 import sys
 
-white_list = set(x.strip() for x in sys.argv[1:])
-
-for line in sys.stdin:
-    if line[0]!="@":
-        #Should be a read
-        qname, flag, rname, pos, mapq, cigar, rnext, pnext, tlen, seq, qual, tags = line.rstrip().split("\t", 11)
-        tags = "\t".join(t for t in tags.split("\t") if t[:2] in white_list)
-        line = "\t".join([qname, flag, rname, pos, mapq, cigar, rnext, pnext, tlen, seq, qual, tags]) + "\n"
-    sys.stdout.write(line)
+if "-v" in sys.argv[1:]:
+    black_list = set(x.strip() for x in sys.argv[1:] if x!="-v")
+    sys.stderr.write("Removing these tags: %s\n" % ", ".join(black_list))
+    for line in sys.stdin:
+        if line[0]!="@":
+            #Should be a read
+            qname, flag, rname, pos, mapq, cigar, rnext, pnext, tlen, seq, qual, tags = line.rstrip().split("\t", 11)
+            tags = "\t".join(t for t in tags.split("\t") if t[:2] not in black_list)
+            line = "\t".join([qname, flag, rname, pos, mapq, cigar, rnext, pnext, tlen, seq, qual, tags]) + "\n"
+        sys.stdout.write(line)
+else:
+    white_list = set(x.strip() for x in sys.argv[1:])
+    sys.stderr.write("Keeping only these tags: %s\n" % ", ".join(white_list))
+    for line in sys.stdin:
+        if line[0]!="@":
+            #Should be a read
+            qname, flag, rname, pos, mapq, cigar, rnext, pnext, tlen, seq, qual, tags = line.rstrip().split("\t", 11)
+            tags = "\t".join(t for t in tags.split("\t") if t[:2] in white_list)
+            line = "\t".join([qname, flag, rname, pos, mapq, cigar, rnext, pnext, tlen, seq, qual, tags]) + "\n"
+        sys.stdout.write(line)
