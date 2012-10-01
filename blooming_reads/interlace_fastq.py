063de4f0f5731a7675bf30183c0812ad64d1d740
peterjc <p.j.a.cock@googlemail.com>
2012-10-01 16:52:18 +0100

    Preserve FASTQ description in interlace script when ID matches

--- blooming_reads/interlace_fastq.py
+++ blooming_reads/interlace_fastq.py
@@ -39,13 +39,16 @@ for title1, seq1, qual1 in iter1:
         title2, seq2, qual2 = iter2.next()
     except StopIteration:
         sys_exit("More records in %s than %s, e.g. %s" % (fastq1, fastq2, title1))
-    id1 = title1.split(None,1)[0]
-    id2 = title2.split(None,1)[0]
+    id1, descr1 = title1.split(None, 1)
+    id2, descr2 = title2.split(None, 1)
     if id1 == id2:
-        #Add the /1 and /2
-        #TODO - Preserve the rest of the title lines?
-        out_handle.write("@%s/1\n%s\n+\n%s\n@%s/2\n%s\n+\n%s\n" \
-                         % (id1, seq1, qual1, id2, seq2, qual2))
+        #Add the /1 and /2, preserve any description after the ID
+        if descr1:
+            descr1 = " " + descr1
+        if descr2:
+            descr2 = " " + descr2
+        out_handle.write("@%s/1%s\n%s\n+\n%s\n@%s/2%s\n%s\n+\n%s\n" \
+                         % (id1, descr1, seq1, qual1, id2, descr2, seq2, qual2))
     elif id1.endswith("/1") and id2.endswith("/2") and id1[:-2]==id2[:-2]:
         out_handle.write("@%s\n%s\n+\n%s\n@%s\n%s\n+\n%s\n" \
                          % (title1, seq1, qual1, title2, seq2, qual2))
