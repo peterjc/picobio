fb4325c0ac8e2f54649bd12ae818fe8938a356ac
peterjc <p.j.a.cock@googlemail.com>
2012-10-24 12:12:51 +0100

    Fix opening of an output file (was testing with stdout only)

--- blooming_reads/re_pair_circular_sam.py
+++ blooming_reads/re_pair_circular_sam.py
@@ -96,7 +96,7 @@ def go(input, output, raw_reads, linear_refs, circular_refs, coverage_file):
     if output is None:
         output_handle = sys.stdout
     elif isinstance(output, basestring):
-        output_handle = open(output)
+        output_handle = open(output, "w")
     else:
         output_handle = output
 
