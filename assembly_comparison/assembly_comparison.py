24a559dcf74468ec6b79b7328f2e97cdc3277148
peterjc <p.j.a.cock@googlemail.com>
2013-08-26 17:43:53 +0100

    Draw reverse hits flipped

--- assembly_comparison/assembly_comparison.py
+++ assembly_comparison/assembly_comparison.py
@@ -72,13 +72,16 @@ gd_record_features = gd_track_for_features.new_set()
 
 
 for contig in SeqIO.parse(assembly_fasta, "fasta"):
-    if len(gd_diagram.tracks) > 10:
-        #For testing, shortcut
-        break
+    #if len(gd_diagram.tracks) > 30:
+    #    #For testing, shortcut
+    #    break
     contig_len = len(contig)
     assert contig_len <= max_len
 
-    blast_hits = blast_results[contig.id]
+    try:
+        blast_hits = blast_results[contig.id]
+    except KeyError:
+        continue
     assert len(blast_hits) == 1
     hit = blast_hits[0]
     assert hit.query_id == contig.id
@@ -97,7 +100,11 @@ for contig in SeqIO.parse(assembly_fasta, "fasta"):
     gd_contig_features = gd_track_for_contig.new_set()
 
     #Add feature for whole contig,
-    offset = blast_hsps[0].hit_start - blast_hsps[0].query_start
+    if blast_hsps[0].hit_strand == -1 and blast_hsps[-1].hit_strand == -1:
+        #Assume whole thing flipped...
+        offset = blast_hsps[0].hit_start - blast_hsps[-1].query_start
+    else:
+        offset = blast_hsps[0].hit_start - blast_hsps[0].query_start
     offset = min(max(0, offset), max_len - contig_len)
     loc = FeatureLocation(offset, offset + contig_len, strand=0)
     gd_contig_features.add_feature(SeqFeature(loc), color=colors.grey, border=colors.black)
@@ -107,13 +114,18 @@ for contig in SeqIO.parse(assembly_fasta, "fasta"):
     for hsp in blast_hsps:
         #print "%s:%i-%i hits %s:%i-%i" % (hsp.query_id, hsp.query_start, hsp.query_end,
         #                                  hsp.hit_id, hsp.hit_start, hsp.hit_end)
-        color = colors.firebrick
+        if hsp.hit_strand == -1:
+            flip = True
+            color = colors.blue
+        else:
+            flip = False
+            color = colors.firebrick
         border = colors.lightgrey
         loc = FeatureLocation(offset + hsp.query_start, offset + hsp.query_end, strand=0)
         q = gd_contig_features.add_feature(SeqFeature(loc), color=color, border=border)
         loc = FeatureLocation(hsp.hit_start, hsp.hit_end, strand=0)
         h = gd_record_features.add_feature(SeqFeature(loc), color=color, border=border)
-        gd_diagram.cross_track_links.append(CrossLink(q, h, color, border))
+        gd_diagram.cross_track_links.append(CrossLink(q, h, color, border, flip))
 
 
 gd_feature_set = gd_track_for_features.new_set()
@@ -126,7 +138,7 @@ for feature in record.features:
                                label_position="start",
                                label_size=6, label_angle=0)
 
-page = ((3 + len(gd_diagram.tracks)) * cm, max_len * cm / 100000.0)
+page = ((3 + 0.5*len(gd_diagram.tracks)) * cm, max_len * cm / 100000.0)
 gd_diagram.draw(format="linear", pagesize=page, fragments=1,
                 start=0, end=max_len)
 gd_diagram.write(diagram_pdf, "PDF")
