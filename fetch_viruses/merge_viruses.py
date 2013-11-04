8232081fec1ec5589a6c12bbd018b8733148a332
peterjc <p.j.a.cock@googlemail.com>
2013-11-04 17:48:26 +0000

    Include de-duplication in merge

--- fetch_viruses/merge_viruses.py
+++ fetch_viruses/merge_viruses.py
@@ -18,6 +18,31 @@ date_stamp = "20131104"
 tables = {"NC_008956":1, "NC_008954":1, "NC_008949":1, "NC_008948":1,
           "NC_011452":1, "NC_008956":1}
 
+def dedup(input_fasta, output_fasta):
+    """Merge identical FASTA entries using NCBI NR Ctrl+A style"""
+    #Try it in memory...
+    print("Deduplicating %s..." % input_fasta)
+    by_seq = dict()
+    total = 0
+    for record in SeqIO.parse(input_fasta, "fasta"):
+        total += 1
+        s = str(record.seq).upper()
+        t = record.description
+        try:
+            by_seq[s].append(t)
+        except KeyError:
+            by_seq[s] = [t]
+    print("Reduced %s from %i to %i records as %s (%0.1f%%)"
+          % (input_fasta, total, len(by_seq), output_fasta,
+             len(by_seq) * 100.0 / float(total)))
+    handle = open(output_fasta, "w")
+    for s in by_seq:
+        titles = by_seq[s]
+        #chr(1) = CTRL+A
+        handle.write(">%s\n%s\n" % (chr(1).join(titles), s))
+    handle.close()
+
+
 def get_nuc(seq, loc_string) :
     reverse = False
     if loc_string.startswith("complement(") :
@@ -42,11 +67,13 @@ for group in ["dsDnaViruses",
     print("="*len(group))
     names = open("GenBank/%s.txt" % group).read().split("\n")
     protein_file = "%s_%s_proteins.faa" % (group, date_stamp)
+    protein_nr = "%s_%s_proteins_NR.faa" % (group, date_stamp)
     nuc_file = "%s_%s_genes.ffn" % (group, date_stamp)
+    nuc_nr = "%s_%s_genes_NR.ffn" % (group, date_stamp)
     print("Looking at %i %s" % (len(names), group))
 
 
-    if os.path.isfile(protein_file) :
+    if os.path.isfile(protein_file):
         print("Got %s" % protein_file)
     else :
         handle = open(protein_file,"w")
@@ -97,6 +124,11 @@ for group in ["dsDnaViruses",
         print("Done")
         print("%i proteins" % count)
         print("%i missing provided translation" % bad)
+
+    if os.path.isfile(protein_nr):
+        print("Got %s" % protein_nr)
+    else:
+        dedup(protein_file, protein_nr)
         
     if os.path.isfile(nuc_file):
         print("Got %s" % nuc_file)
@@ -134,3 +166,8 @@ for group in ["dsDnaViruses",
         handle.close()
         print("Done")
         print("%i genes" % count)
+
+    if os.path.isfile(nuc_nr):
+        print("Got %s" % nuc_nr)
+    else:
+        dedup(nuc_file, nuc_nr)
