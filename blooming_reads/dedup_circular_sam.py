058eac6f76af4274b451e817d5545afa45e0a115
peterjc <p.j.a.cock@googlemail.com>
2014-10-03 10:55:43 +0100

    Reinstate setting POS module circle length
    Trying this with BWA and ignoring mrfast and its problematic output.

--- blooming_reads/dedup_circular_sam.py
+++ blooming_reads/dedup_circular_sam.py
@@ -151,13 +151,7 @@ def go(input, output, paired, linear_refs, circular_refs):
             int_pos = int(pos) - 1
             if length <= int_pos:
                 assert int_pos < length*2, "Have POS %i yet length is %i or %i when doubled!\n%r" % (pos, length, length*2, line)
-                #While wait for mrfast 2.5.0.1 to fix this bug,
-                #https://sourceforge.net/tracker/?func=detail&aid=3574131&group_id=260735&atid=1127386
-                #we'll just ignore reads mapped in the second half.
-                #With mrfast they should all be duplicates anyway.
-                line = input_handle.readline()
-                continue
-                #pos = str(int_pos-length+1) #Modulo circle length 
+                pos = str(int_pos-length+1) #Modulo circle length
         #sys.stderr.write("%s good\n" % qname)
         if qname == cur_read_name:
             #Cache this, as a tuple - ordered to allow sorting on position:
