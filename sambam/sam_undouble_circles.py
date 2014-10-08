4207dc6b62bc708b0699118391b63a5bc8b3fb31
peterjc <p.j.a.cock@googlemail.com>
2014-10-08 13:58:28 +0100

    Add command line API from dedup_circular_sam.py

--- sambam/sam_undouble_circles.py
+++ sambam/sam_undouble_circles.py
@@ -24,6 +24,47 @@ https://github.com/peterjc/picobio
 """
 
 import sys
+import os
+from optparse import OptionParser
+
+def sys_exit(msg, error_level=1):
+    """Print error message to stdout and quit with given error level."""
+    sys.stderr.write("%s\n" % msg)
+    sys.exit(error_level)
+
+VERSION = "0.0.0"
+
+parser = OptionParser(usage="usage: %prog [options]",
+                      version="%prog "+VERSION)
+
+#References
+parser.add_option("-l", "--lref", dest="linear_references",
+                  type="string", metavar="FILE", action="append",
+                  help="FASTA file of linear reference sequence(s). "
+                       "Several files can be given if required.")
+parser.add_option("-c", "--cref", dest="circular_references",
+                  type="string", metavar="FILE", action="append",
+                  help="FASTA file of circular reference sequence(s). "
+                       "Several files can be given if required.")
+#Reads
+parser.add_option("-i", "--input", dest="input_reads",
+                  type="string", metavar="FILE",
+                  help="Input file of SAM format mapped reads to be processed (def. stdin)")
+parser.add_option("-o","--output", dest="output_reads",
+                  type="string", metavar="FILE",
+                  help="Output file for processed SAM format mapping (def. stdout)")
+
+(options, args) = parser.parse_args()
+
+if len(sys.argv)==1:
+    parser.print_help()
+    sys.exit(1)
+
+if (not options.linear_references) and (not options.circular_references):
+    parser.error("You must supply some linear and/or circular references")
+if args:
+    parser.error("No arguments expected")
+
 
 def decode_cigar(cigar):
     """Returns a list of 2-tuples, integer count and operator char."""
@@ -66,13 +107,55 @@ def get_frag(flag):
     else:
         return None
 
+
+
+def get_fasta_ids_and_lengths(fasta_filename):
+    h = open(fasta_filename)
+    name = None
+    length = 0
+    for line in h:
+        if line[0] == ">":
+            if name is not None:
+                yield name, length
+            name = line[1:].split(None, 1)[0]
+            length = 0
+        else:
+            length += len(line.strip())
+    if name is not None:
+        yield name, length
+    h.close()
+
+
+#Load the reference sequence lengths
+ref_len_linear = dict()
+if options.linear_references:
+    for f in options.linear_references:
+        ref_len_linear.update(get_fasta_ids_and_lengths(f))
+    sys.stderr.write("Lengths of %i linear references loaded\n" % len(ref_len_linear))
+ref_len_circles = dict()
+if options.circular_references:
+    for f in options.circular_references:
+        ref_len_circles.update(get_fasta_ids_and_lengths(f))
+    sys.stderr.write("Lengths of %i circular references loaded\n" % len(ref_len_circles))
+
+
+#Open handles
+if options.input_reads:
+    input_handle = open(options.input_reads)
+else:
+    input_handle = sys.stdin
+if options.output_reads:
+    output_handle = open(options.output_reads, "w")
+else:
+    output_handle = sys.stdout
+
 last_seq = None
 last_qual = None
 last_name = None
 last_frag = 0
 count = 0
 mod = 0
-for line in sys.stdin:
+for line in input_handle:
     if line[0]!="@":
         #Should be a read
         count += 1
@@ -99,5 +182,12 @@ for line in sys.stdin:
             last_frag = get_frag(flag)
             last_seq = seq
             last_qual = qual
-    sys.stdout.write(line)
+    output_handle.write(line)
+
+#Close handles
+if options.input_reads:
+    input_handle.close()
+if options.output_reads:
+    output_handle.close()
+
 sys.stderr.write("Modified %i out of %i reads\n" % (mod, count))
